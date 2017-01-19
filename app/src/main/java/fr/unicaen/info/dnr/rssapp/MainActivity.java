a2f2e834efa624135241b1250fb907ca4691e8ff
lcouellan <lcouellan@gmail.com>
2017-01-19 13:30:03 +0100

    Add RSSFeed Object

--- app/src/main/java/fr/unicaen/info/dnr/rssapp/MainActivity.java
+++ app/src/main/java/fr/unicaen/info/dnr/rssapp/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.Menu;
 
 import java.util.List;
 
-import fr.unicaen.info.dnr.rssapp.sqlite.MessageDb;
-import fr.unicaen.info.dnr.rssapp.sqlite.MessageDbOpener;
+import fr.unicaen.info.dnr.rssapp.sqlite.message.MessageDb;
+import fr.unicaen.info.dnr.rssapp.sqlite.message.MessageDbOpener;
+import fr.unicaen.info.dnr.rssapp.sqlite.rssfeed.RSSFeedDb;
+import fr.unicaen.info.dnr.rssapp.sqlite.rssfeed.RSSFeedDbOpener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,22 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MessageDbOpener mDbHelper = new MessageDbOpener(this);
-
-        // Gets the data repository in write mode
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-        MessageDb.add(db,"toto","19 janvier 2017");
-
-        // MessageDb.delete(db,2);
-
-        String[] args = { "toto", "19 janvier 2017" };
-
-        List messages = MessageDb.get(db,args);
-        for ( int i = 0; i < messages.size(); i++) {
-            Log.d("Message : " + i,messages.get(i).toString());
-        }
     }
 
     @Override
---
+++ app/src/main/java/fr/unicaen/info/dnr/rssapp/sqlite/rssfeed/RSSFeedDb.java
@@ -0,0 +1,85 @@
+package fr.unicaen.info.dnr.rssapp.sqlite.rssfeed;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.unicaen.info.dnr.rssapp.fr.unicaen.info.dnr.rssapp.entity.Message;
+import fr.unicaen.info.dnr.rssapp.fr.unicaen.info.dnr.rssapp.entity.RSSFeed;
+
+/**
+ * Created by lenaic on 11/01/2017.
+ */
+
+public final class RSSFeedDb {
+
+    /**
+     * Create a new link from an RSSFeed in our database.
+     * @param db : database.
+     * @param link : Object RSSFeed.
+     */
+    public static void add(SQLiteDatabase db, RSSFeed link) {
+        ContentValues values = new ContentValues();
+        values.put(RSSFeedDbOperation.FeedEntry.COLUMN_NAME_LINK, link.getLink());
+
+        db.insert(RSSFeedDbOperation.FeedEntry.TABLE_NAME, null, values);
+    }
+
+    /**
+     * Delete a link using an id in our database.
+     * @param db : database.
+     * @param id : link identifier.
+     */
+    public static void delete(SQLiteDatabase db, long id) {
+        String selection = RSSFeedDbOperation.FeedEntry._ID + " LIKE ?";
+        String[] selectionArgs = { id+"" };
+        db.delete(RSSFeedDbOperation.FeedEntry.TABLE_NAME, selection, selectionArgs);
+    }
+
+    /**
+     * List all the links from our database.
+     * @param db : database.
+     * @param args : arguments from the where clause.
+     * @return List of Object RSSFeed.
+     */
+    public static List get(SQLiteDatabase db, String[] args) {
+        String[] projection = {
+                RSSFeedDbOperation.FeedEntry._ID,
+                RSSFeedDbOperation.FeedEntry.COLUMN_NAME_LINK,
+        };
+
+        String selection = RSSFeedDbOperation.FeedEntry.COLUMN_NAME_LINK + " = ?";
+
+        String sortOrder =
+                RSSFeedDbOperation.FeedEntry.COLUMN_NAME_LINK + " ASC";
+
+        Cursor cursor = db.query(
+                RSSFeedDbOperation.FeedEntry.TABLE_NAME,                     // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                args,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+
+        List feed = new ArrayList();
+        while(cursor.moveToNext()) {
+            long linkId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(RSSFeedDbOperation.FeedEntry._ID));
+            String linkText = cursor.getString(cursor.getColumnIndexOrThrow(RSSFeedDbOperation.FeedEntry.COLUMN_NAME_LINK));
+            RSSFeed link = new RSSFeed(linkId,linkText);
+            feed.add(link);
+        }
+        cursor.close();
+
+        return feed;
+    }
+
+
+
+
+}
