9eb5a499f02698e242e03e6e072e0d7105f4ff51
Raphael-Erfani <raphael.erfani@gmail.com>
2017-01-19 13:50:47 +0100

    Liste page principale

--- app/src/main/java/fr/unicaen/info/dnr/rssapp/MainActivity.java
+++ app/src/main/java/fr/unicaen/info/dnr/rssapp/MainActivity.java
@@ -5,13 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import fr.unicaen.info.dnr.rssapp.fr.unicaen.info.dnr.rssapp.entity.Message;
 import fr.unicaen.info.dnr.rssapp.sqlite.MessageDb;
 import fr.unicaen.info.dnr.rssapp.sqlite.MessageDbOpener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView rssList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
         String[] args = { "toto", "19 janvier 2017" };
 
+        rssList = (ListView) findViewById(R.id.rssList);
+        String[] rssItems = new String[10];
+
         List messages = MessageDb.get(db,args);
-        for ( int i = 0; i < messages.size(); i++) {
+        for ( int i = 0; i < rssItems.length; i++) {
             Log.d("Message : " + i,messages.get(i).toString());
+            Message message = (Message)messages.get(i);
+            rssItems[i] = messages.get(i).toString();
         }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,rssItems);
+        rssList.setAdapter(adapter);
     }
 
     @Override
