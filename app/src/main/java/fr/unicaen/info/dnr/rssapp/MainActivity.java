d0a9f494bb67ad3f3a7a2d78e0876881f3c34763
yboyer <bhyw180@gmail.com>
2017-01-21 15:14:30 +0100

    Set the 'add feed' activity as an AlertDialog
    Remove the 'show rss links' icon on the main activity's action bar
    Add french translations

--- app/src/main/java/fr/unicaen/info/dnr/rssapp/MainActivity.java
+++ app/src/main/java/fr/unicaen/info/dnr/rssapp/MainActivity.java
@@ -1,13 +1,21 @@
 package fr.unicaen.info.dnr.rssapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.util.Patterns;
 import java.util.List;
 import fr.unicaen.info.dnr.rssapp.entity.RSSItem;
 import fr.unicaen.info.dnr.rssapp.sqlite.rssitem.RSSItemDb;
@@ -17,6 +25,7 @@ import fr.unicaen.info.dnr.rssapp.sqlite.rssitem.RSSItemDbOpener;
 public class MainActivity extends AppCompatActivity {
 
     private ListView rssList;
+    private AlertDialog addFeedDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +58,55 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_add:
-                startActivity(new Intent(this, AddActivity.class));
-                return true;
-            case R.id.menu_show_list:
-                // TODO: show the Feed list
+                openAddFeedDialogBox();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void openAddFeedDialogBox() {
+        View promptView = this.getLayoutInflater().inflate(R.layout.dialog_signin, null);
+
+        final EditText dialogFeedName = (EditText) promptView.findViewById(R.id.dialogFeedName);
+        final EditText dialogFeedUrl = (EditText) promptView.findViewById(R.id.dialogFeedUrl);
+
+        // Watch text events
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override public void afterTextChanged(Editable s) {}
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean dialogFeedNameValid = dialogFeedName.getText().length() != 0;
+                boolean dialogFeedUrlValid = Patterns.WEB_URL.matcher(dialogFeedUrl.getText()).matches();
+
+                // Disable the positive button if the input are invalid
+                addFeedDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(dialogFeedNameValid && dialogFeedUrlValid);
+            }
+        };
+        dialogFeedName.addTextChangedListener(textWatcher);
+        dialogFeedUrl.addTextChangedListener(textWatcher);
+
+        // Create the dialog
+        final AlertDialog.Builder addFeedBuilder = new AlertDialog.Builder(this);
+        addFeedBuilder.setTitle(R.string.addFeed);
+        addFeedBuilder.setView(promptView)
+            .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    // TODO: add in db
+                }
+            })
+            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) { }
+            });
+        addFeedDialog = addFeedBuilder.create();
+
+        addFeedDialog.show();
+        // Disable the positive button
+        addFeedDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
