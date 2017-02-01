641a97bf2b658908dbc4556a2b6638f496a80c78
Raphael-Erfani <raphael.erfani@gmail.com>
2017-02-01 14:10:12 +0100

    Create event modify and delete for the MainActivity

--- app/src/main/java/fr/unicaen/info/dnr/rssapp/adapter/RSSFeedCursorAdapter.java
+++ app/src/main/java/fr/unicaen/info/dnr/rssapp/adapter/RSSFeedCursorAdapter.java
@@ -3,8 +3,10 @@ package fr.unicaen.info.dnr.rssapp.adapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import fr.unicaen.info.dnr.rssapp.R;
 
@@ -19,11 +21,28 @@ public class RSSFeedCursorAdapter extends ResourceCursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(final View view, final Context context, Cursor cursor) {
         TextView name = (TextView) view.findViewById(R.id.name);
         name.setText(cursor.getString(cursor.getColumnIndex("name")));
 
         TextView url = (TextView) view.findViewById(R.id.url);
         url.setText(cursor.getString(cursor.getColumnIndex("url")));
+
+        ImageView edit = (ImageView) view.findViewById(R.id.edit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "click edit", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageView delete = (ImageView) view.findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "click delete", Toast.LENGTH_SHORT).show();
+                
+            }
+        });
     }
 }
