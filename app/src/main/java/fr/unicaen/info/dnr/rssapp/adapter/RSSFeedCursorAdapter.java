41d9190aa672ad53c7dd0cc6298945f85770b367
Raphael-Erfani <raphael.erfani@gmail.com>
2017-02-03 14:20:40 +0100

    Update the update method for the feeds

--- app/src/main/java/fr/unicaen/info/dnr/rssapp/adapter/RSSFeedCursorAdapter.java
+++ app/src/main/java/fr/unicaen/info/dnr/rssapp/adapter/RSSFeedCursorAdapter.java
@@ -10,6 +10,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Patterns;
 import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 import fr.unicaen.info.dnr.rssapp.MainActivity;
 import fr.unicaen.info.dnr.rssapp.R;
 import fr.unicaen.info.dnr.rssapp.RssItemActivity;
+import fr.unicaen.info.dnr.rssapp.entity.RSSFeed;
 import fr.unicaen.info.dnr.rssapp.manager.EntryManager;
 
 
@@ -28,17 +30,22 @@ import fr.unicaen.info.dnr.rssapp.manager.EntryManager;
  */
 
 public class RSSFeedCursorAdapter extends ResourceCursorAdapter {
+
+    private AlertDialog addFeedDialog;
+
     public RSSFeedCursorAdapter(Context context, int layout, Cursor cursor, int flags) {
         super(context, layout, cursor, flags);
     }
 
     @Override
     public void bindView(final View view, final Context context, Cursor cursor) {
-        TextView name = (TextView) view.findViewById(R.id.name);
+        final TextView name = (TextView) view.findViewById(R.id.name);
         name.setText(cursor.getString(cursor.getColumnIndex("name")));
 
-        TextView url = (TextView) view.findViewById(R.id.url);
+        final TextView url = (TextView) view.findViewById(R.id.url);
         url.setText(cursor.getString(cursor.getColumnIndex("url")));
+
+        final Cursor cursor1 = cursor;
 
         ImageView edit = (ImageView) view.findViewById(R.id.edit);
         edit.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +56,62 @@ public class RSSFeedCursorAdapter extends ResourceCursorAdapter {
                 //ouvrir un formulaire de modification de l'item de la rssList a position
                 //puis valider ces changements en BDD
                 notifyDataSetChanged();
+
+                // Get the dialog layout
+                View promptView = LayoutInflater.from(context).inflate(R.layout.dialog_modify, null);
+
+                final EditText dialogFeedName = (EditText) promptView.findViewById(R.id.modifyDialogFeedName);
+                final EditText dialogFeedUrl = (EditText) promptView.findViewById(R.id.modifyDialogFeedUrl);
+
+                dialogFeedName.setText(cursor1.getString(cursor1.getColumnIndex("name")));
+                dialogFeedUrl.setText(cursor1.getString(cursor1.getColumnIndex("url")));
+
+                // Watch text events
+                TextWatcher textWatcher = new TextWatcher() {
+                    @Override public void afterTextChanged(Editable s) {}
+                    @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        boolean dialogFeedNameValid = dialogFeedName.getText().length() != 0;
+                        boolean dialogFeedUrlValid = Patterns.WEB_URL.matcher(dialogFeedUrl.getText()).matches();
+
+                        // Disable the positive button if inputs are invalid
+                        addFeedDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(dialogFeedNameValid && dialogFeedUrlValid);
+                    }
+                };
+                dialogFeedName.addTextChangedListener(textWatcher);
+                dialogFeedUrl.addTextChangedListener(textWatcher);
+
+                //Create and show the dialog code
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setTitle("Modification");
+
+                // set dialog message
+                alertDialogBuilder.setView(promptView)
+                        .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                String name = dialogFeedName.getText().toString();
+                                String url = dialogFeedUrl.getText().toString();
+
+                                // Add http if the url does not contains it
+                                if (!url.startsWith("http")) {
+                                    url = "http://" + url;
+                                }
+
+                                // Modify feed in DB
+                                //new EntryManager(context).upsert());
+                                Toast.makeText(context, "Clic valider", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.cancel, null);
+                addFeedDialog = alertDialogBuilder.create();
+
+                addFeedDialog.show();
+                // Disable the positive button
+                addFeedDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+
             }
         });
 
